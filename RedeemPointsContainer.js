9664fe1ee988996b6b0903ac1c171a4bd0219f11
SanjaiBai <sanjaikumar.km@valuehealthsol.com>
2025-06-03 11:22:11 +0530

    Create RedeemPointsContainer.js

---
+++ RedeemPointsContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import RedeemPoints from '../components/RedeemPoints';
+
+const RedeemPointsContainer = () => {
+  const pointBalance = 1200;
+
+  return <RedeemPoints pointBalance={pointBalance} />;
+};
+
+export default RedeemPointsContainer;
