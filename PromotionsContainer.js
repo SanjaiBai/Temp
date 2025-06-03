da3072c4e5c002a6672b17a7e402fa8519900fd5
SanjaiBai <sanjaikumar.km@valuehealthsol.com>
2025-06-03 11:24:05 +0530

    Create PromotionsContainer.js

---
+++ PromotionsContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Promotions from '../components/Promotions';
+
+const PromotionsContainer = () => {
+  const promotions = [
+    { title: 'Spring Special', description: 'Double points on all purchases in April!' },
+    { title: 'Dental Week Bonus', description: 'Extra 300 points for dental products' },
+  ];
+
+  return <Promotions promotions={promotions} />;
+};
+
+export default PromotionsContainer;
